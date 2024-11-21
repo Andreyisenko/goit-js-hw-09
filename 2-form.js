8b01c4a6dc71457d9833bdda498d279f0020130d
Andreyisenko <aisenko87@gmail.com>
2024-11-21 06:25:56 +0000

    Deploying to gh-pages from @ Andreyisenko/goit-js-hw-09@a730ad8458edb46cbbf19507262bd9ec178ffaeb 🚀

--- 2-form.js
+++
@@ -1,2 +0,0 @@
-import"./assets/styles--YRFfz2g.js";const o=document.querySelector(".feedback-form");console.log(o);const e=document.querySelector("button");console.log(e);
-//# sourceMappingURL=2-form.js.map
